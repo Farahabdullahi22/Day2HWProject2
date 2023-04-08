9c896b3a70ca45132d9f28b88fd21a69003868ae
farahabdullahi <farahabdullahi@192.168.4.25>
2023-04-08 01:57:12 -0400

    Moving stash two to remote repository

--- src/day2/Stash2.java
+++ src/day2/Stash2.java
@@ -5,6 +5,11 @@ public class Stash2 {
 	public static void main(String[] args) {
 		
 		System.out.println("Stash two practice ");
+		
 	}
 
+	public void subtract() {
+		
+		System.out.println("22- 12");
+	}
 }
